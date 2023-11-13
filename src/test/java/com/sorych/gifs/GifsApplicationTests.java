4b260050cccc554630d5d77cdf251db5096af161
sorych <dm.sorych@gmail.com>
2023-11-13 13:31:28 -0800

    Init Spring boot app

---
+++ src/test/java/com/sorych/gifs/GifsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorych.gifs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
