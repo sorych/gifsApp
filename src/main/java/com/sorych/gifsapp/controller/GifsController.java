c2d6913b9a9b775fdfd1d26132a5ece6f7ee4972
sorych <dm.sorych@gmail.com>
2023-11-15 13:57:39 -0800

    returning bad request if max param count exceeded

--- src/main/java/com/sorych/gifsapp/controller/GifsController.java
+++ src/main/java/com/sorych/gifsapp/controller/GifsController.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,20 +20,29 @@ public class GifsController {
 
   private GifsService gifsService;
 
+  private int maxParameterCount;
+
   private static final Logger logger = LoggerFactory.getLogger(GifsController.class);
 
   @Autowired
-  public GifsController(GifsService gifsService) {
+  public GifsController(
+      GifsService gifsService,
+      @Value("${server.tomcat.max-parameter-count:10000}") Integer maxParameterCount) {
     this.gifsService = gifsService;
+    this.maxParameterCount = maxParameterCount;
   }
 
   @GetMapping("/query")
   public ResponseEntity<GifsControllerResponse> query(@RequestParam List<String> searchTerm) {
     if (searchTerm.isEmpty()) {
-      logger.debug("SearchTerms are empty");
+      logger.info("SearchTerms are empty");
       return ResponseEntity.badRequest().build();
     }
     List<String> uniqueTerms = searchTerm.stream().distinct().collect(Collectors.toList());
+    if (uniqueTerms.size() > maxParameterCount) {
+      logger.info("received too much params");
+      return ResponseEntity.badRequest().build();
+    }
     List<SearchResult> searchResults = gifsService.findGifs(uniqueTerms);
     return ResponseEntity.ok(ControllerResponseBuilder.buildResponse(searchResults));
   }
